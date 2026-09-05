25b7f0b73e12dcdb3dde413098011b8e878ad295
Eduardo Reichert <eduardobasanezreichert@gmail.com>
2026-09-05 18:18:23 -0300

    Delete metodos_pesquisa.h

--- metodos_pesquisa.h
+++
@@ -1,8 +0,0 @@
-#ifndef METODOS_DE_PESQUISA
-#define METODOS_DE_PESQUISA
-
-void pesquisa_DFS(int qtd_vertice, int matriz[qtd_vertice][qtd_vertice], int vet_visitado[qtd_vertice], int vertice_inicio);
-
-void pesquisa_BFS(int qtd_vertice, int matriz[qtd_vertice][qtd_vertice], int vet_visitado[qtd_vertice], int vertice_inicio);
-
-#endif
